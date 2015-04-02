bab1210d89432b57294df184b0009183cc536c30
Sawyer Hollenshead <s.hollenshead@gmail.com>
2015-04-02 17:00:53 -0400

    Liquid syntax

---
+++ src/languages/liquid.js
@@ -0,0 +1,37 @@
+/*
+Language: Liquid
+Requires: xml.js
+Author: Sawyer Hollenshead <hello@sawyerh.com>
+Description: Matcher for Liquid as well as Siteleaf additions.
+Category: template
+*/
+
+function(hljs) {
+  var EXPRESSION_KEYWORDS = 'for each in with if else elsif endif endunless endfor unless include capture';
+  return {
+    aliases: ['liquid', 'html.liquid', 'html.liquid'],
+    case_insensitive: true,
+    subLanguage: 'xml', subLanguageMode: 'continuous',
+    contains: [
+      {
+        className: 'expression',
+        variants: [
+            {begin: '{{', end: '}}'},
+            {begin: '{%', end: '%}'}
+        ],
+        keywords: EXPRESSION_KEYWORDS,
+        contains: [
+          {
+            className: 'string',
+            begin: '"|\'', end: '"|\''
+          },
+          {
+            className: 'variable',
+            begin: '[a-zA-Z\-\.\_]+',
+            keywords: EXPRESSION_KEYWORDS
+          }
+        ]
+      }
+    ]
+  };
+}
